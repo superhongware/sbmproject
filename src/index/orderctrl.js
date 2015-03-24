4d20d11032e65fc761f45057650b50bdabbde0e8
Charles <hzher@qq.com>
2015-03-24 16:41:01 +0800

    订单页面样式，及相关操作方法绑定

--- src/index/orderctrl.js
+++ src/index/orderctrl.js
@@ -1,19 +1,67 @@
 starterctrl.controller('ordersCtrl', ['$scope', '$ionicPopover', '$http', '$ionicLoading', function($scope, $ionicPopover, $http, $ionicLoading) {
 
+    $scope.moduleOption = {
+
+    };
+
     $scope.shopList = [{
-        name: '上海百货'
+        name: '上海百货',
+        value: '123'
     }, {
-        name: '南宁百货'
+        name: '南宁百货',
+        value: '456'
     }];
 
     $scope.orderList = [{
-
+        picUrl: 'http://zhaoyanblog.com/wp-content/uploads/2014/03/20140319215736751.png',
+        title: '小西装',
+        status: 'PAID',
+        totalAmount: '233',
+        orderCount: '100',
+        shopName: '上海百货',
+        plat: '天猫' //平台类型
     }];
 
+    $scope.orderStatusList = [{
+        name: '未付款',
+        status: 'NON_PAYMENT'
+    }, {
+        name: '已付款',
+        status: 'PAID'
+    }, {
+        name: '已打印',
+        status: 'PRINTED'
+    }, {
+        name: '已发货',
+        status: 'DELIVERED'
+    }, {
+        name: '已取消',
+        status: 'CANCELED'
+    }, {
+        name: '已完成',
+        status: 'COMPLETED'
+    }];
+
+    $scope.showOrderStatusList = function() {
+        var arr = [];
+        for (var i = 0; i < $scope.orderStatusList.length; i++) {
+            if ($scope.currOrderStatusName != $scope.orderStatusList[i].name) {
+                arr.push($scope.orderStatusList[i]);
+            }
+        }
+        return arr;
+    };
+
+    $scope.currOrderStatusName = $scope.orderStatusList[0].name;
+
+    /**
+     * [refreshServer 刷新远程数据]
+     * @return {[type]}
+     */
     $scope.refreshServer = function() {
 
         $ionicLoading.show({
-            template: "正在同步123..."
+            template: "正在同步..."
         });
 
         setTimeout(function() {
@@ -23,8 +71,28 @@ starterctrl.controller('ordersCtrl', ['$scope', '$ionicPopover', '$http', '$ioni
     };
 
 
+    /**
+     * [refreshOrderList 上拉刷新]
+     * @return {[type]}
+     */
     $scope.refreshOrderList = function() {
+
         console.log('Refreshing!');
+
+
+        for (var i = 0; i < 5; i++) {
+            $scope.orderList.push({
+                picUrl: 'http://zhaoyanblog.com/wp-content/uploads/2014/03/20140319215736751.png',
+                title: '小西装',
+                status: 'PAID',
+                totalAmount: '233',
+                orderCount: '100',
+                shopName: '上海百货',
+                plat: '天猫' //平台类型
+            });
+        }
+        
+
         setTimeout(function() {
             $scope.$broadcast('scroll.refreshComplete');
         }, 1000);
@@ -32,65 +100,79 @@ starterctrl.controller('ordersCtrl', ['$scope', '$ionicPopover', '$http', '$ioni
     };
 
 
-    var template = '<ion-popover-view><ion-content><ion-list><ion-item>未付款</ion-item><ion-item>已打印</ion-item><ion-item>未已发货</ion-item></ion-list> </ion-content></ion-popover-view>';
+    $scope.loadMoreData = function() {
+        console.log('loadMoreData');
 
-    $scope.popover = $ionicPopover.fromTemplate(template, {
-        scope: $scope
-    });
+        for (var i = 0; i < 5; i++) {
+            $scope.orderList.push({
+                picUrl: 'http://zhaoyanblog.com/wp-content/uploads/2014/03/20140319215736751.png',
+                title: '小西装',
+                status: 'PAID',
+                totalAmount: '233',
+                orderCount: '100',
+                shopName: '上海百货',
+                plat: '天猫' //平台类型
+            });
+        }
 
+        $scope.$broadcast('scroll.infiniteScrollComplete');
 
-    $scope.openPopover = function($event) {
-        $scope.popover.show($event);
     };
-    $scope.closePopover = function() {
+
+    $scope.loadDataByStatusFilter = function(item) {
+        $scope.currOrderStatusName = item.name;
         $scope.popover.hide();
-    };
-    //Cleanup the popover when we're done with it!
-    $scope.$on('$destroy', function() {
-        $scope.popover.remove();
-    });
-    // Execute action on hide popover
-    $scope.$on('popover.hidden', function() {
-        console.log('popover.hidden');
-    });
-    // Execute action on remove popover
-    $scope.$on('popover.removed', function() {
-        console.log('popover.removed');
-    });
 
-    $scope.orderFilterShowEvent = function() {
-        console.log('orderFilterShowEvent');
+        $ionicLoading.show({
+            template: "正在加载..."
+        });
+
+        setTimeout(function() {
+            $ionicLoading.hide();
+        }, 1000);
     };
 
+    $scope.loadDataByShop = function(item) {
+        $ionicLoading.show({
+            template: "正在加载..."
+        });
 
-    // $scope.doRefresh = function () {
-    //     console.log(123123);
-    //     return true;
-    // };
+        console.log(item.value);
 
-    // $scope.openPopover = function ($event) {
+        setTimeout(function() {
+            $ionicLoading.hide();
+        }, 1000);
+    };
+
+    $ionicPopover.fromTemplateUrl('/templates/index/order/orderStatusfilterPopover.html', {
+        scope: $scope,
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
+
+
+    $scope.$on('$stateChangeSuccess', function() {
+        $scope.loadMoreData();
+    });
+
+
+    // $scope.openPopover = function($event) {
     //     $scope.popover.show($event);
     // };
-    // $scope.closePopover = function () {
+    // $scope.closePopover = function() {
     //     $scope.popover.hide();
     // };
     // //Cleanup the popover when we're done with it!
-    // $scope.$on('$destroy', function () {
-    //     console.log("$destroy");
+    // $scope.$on('$destroy', function() {
     //     $scope.popover.remove();
     // });
     // // Execute action on hide popover
-    // $scope.$on('popover.hidden', function () {
-    //     console.log("popover.hidden");
-
-    //     // Execute action
+    // $scope.$on('popover.hidden', function() {
+    //     console.log('popover.hidden');
     // });
     // // Execute action on remove popover
-    // $scope.$on('popover.removed', function () {
-    //     console.log("popover.removed");
-
-    //     // Execute action
+    // $scope.$on('popover.removed', function() {
+    //     console.log('popover.removed');
     // });
-
 
 }]);
