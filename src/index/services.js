9249da0e8a98f8daf8b0297cb1fefc1085478438
Charles <hzher@qq.com>
2015-03-26 19:34:13 +0800

    增加时间格式化方法

--- src/index/services.js
+++ src/index/services.js
@@ -28,6 +28,36 @@
 	};
 })
 
+.factory('dateFormat', function() {
+	/**
+	 * [时间格式化]
+	 * @param  {[type]} date   [格式化的时间对象]
+	 * @param  {[type]} format [格式]
+	 * @return {[type]}        [description]
+	 */
+	return function(date,format) {
+		var o = {
+			"M+": date.getMonth() + 1, //month 
+			"d+": date.getDate(), //day 
+			"h+": date.getHours(), //hour 
+			"m+": date.getMinutes(), //minute 
+			"s+": date.getSeconds(), //second 
+			"q+": Math.floor((date.getMonth() + 3) / 3), //quarter 
+			"S": date.getMilliseconds() //millisecond 
+		}
+		if (/(y+)/.test(format)) {
+			format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+		}
+
+		for (var k in o) {
+			if (new RegExp("(" + k + ")").test(format)) {
+				format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+			}
+		}
+		return format;
+	};
+})
+
 
 .factory('loginCheck',['$state','myCookie','base64',function($state,myCookie,base64){
 	return function(){
