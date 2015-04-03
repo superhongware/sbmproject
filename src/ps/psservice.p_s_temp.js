956c0dcef18e278e652e049459c44607d70c3620
Charles <hzher@qq.com>
2015-04-03 16:20:23 +0800

    动画

--- src/ps/psservice.p_s_temp.js
+++ src/ps/psservice.p_s_temp.js
@@ -1,58 +1,87 @@
-SBMPS.factory('p_s_temp',['p_s_anination', function(p_s_anination){
-	var p_s_temp=function(page){
-		var temp=page[0].className.match(/tmp\w*\s?/)[0].replace(/tmp/,"");
+SBMPS.factory('p_s_temp', ['p_s_anination', function(p_s_anination) {
+	var p_s_temp = function(page) {
+		var temp = page[0].className.match(/tmp\w*\s?/)[0].replace(/tmp/, "");
 
-		switch(temp){
+		switch (temp) {
 			case "ht1":
 
-				p_s_anination(page,".ps_img3","psanimate2",function(){
-					p_s_anination(page,".ps_img2","psanimate15");
-					p_s_anination(page,".ps_text1","psanimate2");
+				p_s_anination(page, ".ps_img3", "psanimate2", function() {
+					p_s_anination(page, ".ps_img2", "psanimate15");
+					p_s_anination(page, ".ps_text1", "psanimate2");
 				});
 
-			break;
+				break;
 			case "ht2":
-				p_s_anination(page,".ps_text1","psanimate15");
-				p_s_anination(page,".ps_text2","psanimate15",function(){
-					p_s_anination(page,".ps_text3","psanimate2");
+				p_s_anination(page, ".ps_text1", "psanimate15");
+				p_s_anination(page, ".ps_text2", "psanimate15", function() {
+					p_s_anination(page, ".ps_text3", "psanimate2");
 				});
-			break;
+				break;
 			case "ht3":
-				p_s_anination(page,".ps_text1","psanimate8_1",function(){
-					p_s_anination(page,".ps_text2","psanimate8_1");
+				p_s_anination(page, ".ps_text1", "psanimate8_1", function() {
+					p_s_anination(page, ".ps_text2", "psanimate8_1");
 				});
-			break;
+				break;
 			case "ht4":
-				p_s_anination(page,".ps_text1","psanimateFadeInDown",function(){
-					p_s_anination(page,".ps_text2","psanimateFadeInDown");
+				p_s_anination(page, ".ps_text1", "psanimateFadeInDown", function() {
+					p_s_anination(page, ".ps_text2", "psanimateFadeInDown");
 				});
-			break;
+				break;
+			case "ht5":
+				p_s_anination2(".divImgWarp0 .ps_img_s0", "psanimateFadeInDown_1", function() {
+					p_s_anination2(".divImgWarp1 .ps_img_s0", "psanimateFadeInLeft", function() {
+						p_s_anination2(".divImgWarp1 .ps_img_s1", "psanimateFadeInRight", function() {
+							p_s_anination2(".tmpht5 .ps_text1", "psanimateFadeInDown_1", function() {
+								p_s_anination2(".tmpht5 .ps_text2", "psanimateFadeInDown_1", function() {
 
+								});
+							});
+						});
+					});
+				});
+
+				break;
 			default:
 				// console.log("1"+temp);
 
-			break;
+				break;
 		}
 
 	};
+
+	var p_s_anination2 = function(classname, animate, callback) {
+
+		var $dom = $(classname);
+		$dom.addClass(animate);
+		$dom.on("webkitAnimationEnd", function() {
+
+			var $this = $(this);
+			$this.off("webkitAnimationEnd");
+			$this.addClass(animate + "end");
+			if (typeof callback === "function") {
+				callback();
+			}
+
+		});
+	};
+
 	return p_s_temp;
 }])
 
-.factory('p_s_anination', function(){
-	var p_s_anination=function(page,classname,animate,callback){
+.factory('p_s_anination', function() {
+	var p_s_anination = function(page, classname, animate, callback) {
 
-				page.children(classname).addClass(animate);
+		page.children(classname).addClass(animate);
 
-				page.children(classname).on("webkitAnimationEnd",function(){
+		page.children(classname).on("webkitAnimationEnd", function() {
 
-					$(this).off("webkitAnimationEnd");
-					$(this).addClass(animate+"end");
-					if(typeof callback === "function"){
-						callback();
-					}
+			$(this).off("webkitAnimationEnd");
+			$(this).addClass(animate + "end");
+			if (typeof callback === "function") {
+				callback();
+			}
 
-				});
-
+		});
 	};
 	return p_s_anination;
 });
