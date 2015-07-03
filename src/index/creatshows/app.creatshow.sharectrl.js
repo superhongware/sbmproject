8522c28d5f53d0c2f7664376d1e7e2df4667f673
hldzlk <545264278@qq.com>
2015-07-03 20:08:13 +0800

    分享

--- src/index/creatshows/app.creatshow.sharectrl.js
+++ src/index/creatshows/app.creatshow.sharectrl.js
@@ -199,31 +199,32 @@ function($http,$scope,$rootScope,$stateParams,$ionicLoading,$state,sendShowImg,S
 // 	}
 // 	$scope.showjuhua=-$scope.showjuhua;
 // }
-//     Tida.ready({
-//         interactId:"", // 互动实列ID type string 若无抽奖模块，此参数无须传入。给错误的实例ID会走错误流程
-//         module: ['draw','sensor','buy','device','social','widget'] // 应用所需要的模块。默认加载所有模块。支持的模块有draw:抽奖;sensor:传感器;buy:交易;device:基础设备接口 social:社交相关 widget:客户端UI
-//     }, function(){
-//         console.log(Tida);
-//         Tida.doAuth(function(data){
-//             if(data.finish){
-//                 alert("授权成功")
-//             // 授权成功 可以顺利调用需要授权的接口了
-//             }else {
-//             // 未能成功授权
-//             }
-//         });
-//         Tida.share({
-//             title:"分享的标题", // 分享标题 在来往和微信好友中有标题显示
-//             content: "分享内容", //分享的内容
-//             url: "http://192.168.1.213/sbmproject/ps.html?orgname=work&detailid=987984&productid=44296753642&plat=TAOBAO", // 跳转地址，分享的内容跳转的url
-//             image:"http://192.168.1.213/sbmproject/img/shareimg.jpg", // 图片地址,客户端可能需要根据url下载图片再分享
-//             wxIsAvailable: 1, // 是否添加微信通道。为真时添加微信入口。仅手淘支持 取值为真或假
-//             shareType: 1, // 为1时只能分享到通讯录。仅手淘支持
-//             wxImage: 1, // 天猫IOS客户端分享到微信/微信朋友圈的图片有限制，这里默认自动转为200x200的。若不是正方形图片且需要自己传递该图片,并遵循天猫客户端图片的限制规则（不要过大 阿里的CDN上的）
-//         }, function(data){
-//         // data.errorCode 为0时分享成功
-//         });
-//     });
+	$scope.share=function(){
+
+
+	        Tida.share({
+	            title:"分享的标题", // 分享标题 在来往和微信好友中有标题显示
+	            content: "分享内容", //分享的内容
+	            url: "http://192.168.1.213/sbmproject/ps.html?orgname=work&detailid=987984&productid=44296753642&plat=TAOBAO", // 跳转地址，分享的内容跳转的url
+	            image:"http://192.168.1.213/sbmproject/img/shareimg.jpg", // 图片地址,客户端可能需要根据url下载图片再分享
+	            wxIsAvailable: 1, // 是否添加微信通道。为真时添加微信入口。仅手淘支持 取值为真或假
+	            // shareType: 1, // 为1时只能分享到通讯录。仅手淘支持
+				wxImage: 1, // 天猫IOS客户端分享到微信/微信朋友圈的图片有限制，这里默认自动转为200x200的。若不是正方形图片且需要自己传递该图片,并遵循天猫客户端图片的限制规则（不要过大 阿里的CDN上的）
+				//渠道控制新增参数
+				'targets' : [ 0, 1, 2, 3, 4, 5, 6, 7],//新的控制分享渠道的参数，以前的参数weixinshare等将逐渐废弃，请不要使用。参数值： 0 通讯录；1 复制；2 微博；3 微信；4 朋友圈；5 二维码；6 来往；7 旺信；
+				//微信分享新增参数
+				'weixinAppId' : "wxf475808481f0620a",//微信分享appId
+				'weixinMsgType':"webpage",//微信分享方式：text文案分享（title必传）、image图片分享（image必传）、webpage图文分享（title、content、image、url必传）
+
+	        }, function(data){
+	        // data.errorCode 为0时分享成功
+	        });
+		
+	}
+
+
+
+
 
 }])
 
