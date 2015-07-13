1848f2549e9e8b29655a540c1d358bed501c1492
hldzlk <zhoulink90@gmail.com>
2015-07-13 16:07:51 +0800

    preview改图片，注册优化

--- src/index/login/loginctrl.js
+++ src/index/login/loginctrl.js
@@ -99,38 +99,60 @@ loginmodule.controller('LoginCtrl', ['$scope', '$rootScope', '$http','loginSubmi
 		channel:"app"
 	};
 	$scope.sign_up = function() {
+		
 		if(($scope.yourdata.password!=="")&&($scope.yourdata.password!==$scope.yourdata.password1)){
 			$(".error-tip").eq(3).show();
+			return;
 		}
+
 		var reg0 =  /^1\d{10}$/;
-	
 		if(!reg0.test($scope.yourdata.phone)&&($scope.yourdata.phone!=="")){
 			$(".error-tip").eq(4).show();
+			return;
 		}
+
 		var  reg= /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
 		if(!reg.test($scope.yourdata.email)&&($scope.yourdata.email!=="")){
 			$(".error-tip").eq(5).show();
+			return;
 		}
-		var restring= /^[\u4e00-\u9fa5a-z0-9_.@]+$/gi;//只能输入汉字和英文字母
-		
-		if(!restring.test($scope.yourdata.orgName)){
-			alert(1)
-			$(".error-tip").eq(0).children(".rect").text("不能输入特殊字符");
-			$(".error-tip").eq(0).show();
-		}
-	var restring2= /^[\u4e00-\u9fa5a-z0-9_.@]+$/gi;//只能输入汉字和英文字母
-		if(!restring2.test($scope.yourdata.userName)){
 
-			$(".error-tip").eq(1).children(".rect").text("不能输入特殊字符");
-			$(".error-tip").eq(1).show();
+
+		if($scope.yourdata.orgName===''){
+			// alert(1)
+			$(".error-tip").eq(0).children(".rect").text("请输入商家名称");
+			$(".error-tip").eq(0).show();
+			return;
 		}
+
+		if($scope.yourdata.userName===''){
+			$(".error-tip").eq(1).children(".rect").text("请输入用户名");
+			$(".error-tip").eq(1).show();
+			return;
+		}
+
+		// var restring= /^[\u4e00-\u9fa5a-z0-9_.@]+$/gi;//只能输入汉字和英文字母		
+		// if(!restring.test($scope.yourdata.orgName)){
+		// 	// alert(1)
+		// 	$(".error-tip").eq(0).children(".rect").text("不能输入特殊字符");
+		// 	$(".error-tip").eq(0).show();
+		// 	return;
+		// }
+
+		// var restring2= /^[\u4e00-\u9fa5a-z0-9_.@]+$/gi;//只能输入汉字和英文字母
+		// if(!restring2.test($scope.yourdata.userName)){
+		// 	$(".error-tip").eq(1).children(".rect").text("不能输入特殊字符");
+		// 	$(".error-tip").eq(1).show();
+		// 	return;
+		// }
+
 		$scope.yourdata.method = "softbanana.app.user.regist";
 		var api = SBMJSONP("registUser", $scope.yourdata);
 		console.log(api);
 		$http.jsonp(api.url)
 			.success(function(data) {
 				if(data.isSuccess){
-					$ionicPopup.show({
+					var siginsuccess=$ionicPopup.show({
 						title: "注册成功",
 						template: "注册消息已发送到邮箱，请妥善保管！",
 						buttons: [{
@@ -138,30 +160,45 @@ loginmodule.controller('LoginCtrl', ['$scope', '$rootScope', '$http','loginSubmi
 							type: "button-energized",
 						}]
 					});
+					siginsuccess.then(function(res){
+						$state.go("login");
+					})
 				}else{
 					console.log(data);
-					if(data.map.errorMsg === "商家名称不允许为空"){
-						$(".error-tip").eq(0).children(".rect").text(data.map.errorMsg);
-						$(".error-tip").eq(0).show();
-					}else if(data.map.errorMsg === "该商家名称已注册"){
-						$(".error-tip").eq(0).children(".rect").text(data.map.errorMsg);
-						$(".error-tip").eq(0).show();
-					}else if(data.map.errorMsg === "用户名不允许为空"){
-						$(".error-tip").eq(1).children(".rect").text(data.map.errorMsg);
-						$(".error-tip").eq(1).show();
-					}else if(data.map.errorMsg === "该商家名称下已注册该用户名"){
-						$(".error-tip").eq(1).children(".rect").text(data.map.errorMsg);
-						$(".error-tip").eq(1).show();
-					}else if(data.map.errorMsg === "密码不允许为空"){
-						$(".error-tip").eq(2).children(".rect").text(data.map.errorMsg);
-						$(".error-tip").eq(2).show();
-					}else if(data.map.errorMsg === "手机号不允许为空"){
-						$(".error-tip").eq(4).children(".rect").text(data.map.errorMsg);
-						$(".error-tip").eq(4).show();
-					}else if(data.map.errorMsg === "Email不允许为空"){
-						$(".error-tip").eq(5).children(".rect").text(data.map.errorMsg);
-						$(".error-tip").eq(5).show();
-					}
+
+					$ionicPopup.show({
+						title: "注册失败",
+						template: data.map.errorMsg,
+						buttons: [{
+							text: '去改下资料',
+							type: "button-energized",
+						}]
+					});
+					// if(data.map.errorMsg === "商家名称不允许为空"){
+					// 	$(".error-tip").eq(0).children(".rect").text(data.map.errorMsg);
+					// 	$(".error-tip").eq(0).show();
+					// }else if(data.map.errorMsg==="商家名称重复！"){
+					// 	$(".error-tip").eq(0).children(".rect").text(data.map.errorMsg);
+					// 	$(".error-tip").eq(0).show();
+					// }else if(data.map.errorMsg === "该商家名称已注册"){
+					// 	$(".error-tip").eq(0).children(".rect").text(data.map.errorMsg);
+					// 	$(".error-tip").eq(0).show();
+					// }else if(data.map.errorMsg === "用户名不允许为空"){
+					// 	$(".error-tip").eq(1).children(".rect").text(data.map.errorMsg);
+					// 	$(".error-tip").eq(1).show();
+					// }else if(data.map.errorMsg === "该商家名称下已注册该用户名"){
+					// 	$(".error-tip").eq(1).children(".rect").text(data.map.errorMsg);
+					// 	$(".error-tip").eq(1).show();
+					// }else if(data.map.errorMsg === "密码不允许为空"){
+					// 	$(".error-tip").eq(2).children(".rect").text(data.map.errorMsg);
+					// 	$(".error-tip").eq(2).show();
+					// }else if(data.map.errorMsg === "手机号不允许为空"){
+					// 	$(".error-tip").eq(4).children(".rect").text(data.map.errorMsg);
+					// 	$(".error-tip").eq(4).show();
+					// }else if(data.map.errorMsg === "Email不允许为空"){
+					// 	$(".error-tip").eq(5).children(".rect").text(data.map.errorMsg);
+					// 	$(".error-tip").eq(5).show();
+					// }
 				}
 				
 			})
